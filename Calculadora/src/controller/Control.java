431e09eb5ba1aed5209cb7955226173f8d3cb94f
Weslley Pereira <hardwess@gmail.com>
2017-09-22 21:19:05 -0300

    Update Control.java

--- Calculadora/src/controller/Control.java
+++ Calculadora/src/controller/Control.java
@@ -1,10 +1,8 @@
 package controller;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 
-import javax.crypto.SecretKeyFactorySpi;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
